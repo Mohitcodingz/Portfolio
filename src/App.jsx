7d5248e82958d4ea919aa9948614d0d962b04c9e
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-07-25 02:46:49 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import Hero from './components/Hero/hero'
+import Hero from './components/Hero/Hero'
  
 
 export default function App() {
