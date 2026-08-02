e1c71d2e05e101763ce6071293e28a53aa73ea11
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-08-02 23:30:14 +0530

    done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
+// import React from 'react'
+// import styles from './Navbar.module.css'
+// import Resume from '../../assets/MohitFullStackDev.pdf'
+// // ../ takes one folder back
+// export default function Navbar() {
+//     return (
+//         <div className={styles.NavbarContainer}>
+//             <h1><span style={{ color: "#7CFF00", fontWeight: "bold" }}>&lt;/&gt;</span>&nbsp;Mohit Singh</h1>
+//             <div className={styles.NavLinks}>
+//                 <a href="#Home">Home</a>
+//                 <a href="#About">About</a>
+//                 <a href="#Skills">Skils</a>
+//                 <a href="#Projects">Projects</a>
+//                 <a href="#Experience">Experience</a>
+//                 <a href="#Education">Education</a>
+//                 <a href="#Contact">Contact</a>
+//             </div>
+//             <div className={styles.DownloadResume}>
+//                 <a href={Resume} >Download CV</a>
+//             </div>
+//         </div>
+//     )
+// }
+
+
 import React from 'react'
 import styles from './Navbar.module.css'
 import Resume from '../../assets/MohitFullStackDev.pdf'
-// ../ takes one folder back
+
 export default function Navbar() {
     return (
         <div className={styles.NavbarContainer}>
@@ -9,17 +34,15 @@ export default function Navbar() {
             <div className={styles.NavLinks}>
                 <a href="#Home">Home</a>
                 <a href="#About">About</a>
-                <a href="#Skills">Skils</a>
+                <a href="#Skills">Skills</a>
                 <a href="#Projects">Projects</a>
                 <a href="#Experience">Experience</a>
                 <a href="#Education">Education</a>
                 <a href="#Contact">Contact</a>
             </div>
             <div className={styles.DownloadResume}>
-                <a href={Resume} >Download CV</a>
+                <a href={Resume}>Download CV</a>
             </div>
         </div>
     )
 }
-
-
