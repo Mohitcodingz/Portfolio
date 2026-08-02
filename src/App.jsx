a933f7eb308039fb031828af6ab9a51ad779e526
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-08-03 01:11:40 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from 'react' 
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
@@ -8,12 +7,13 @@ import Experience from './components/Experience/Experience'
 import Education from './components/Education/Education'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
  
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
+     <Navbar/>
       <Hero/>
       <About/>
       <Skills/>
